c786bf17330d9e6fc9e506a008855c9c8029af97
Matt <matt@builtbyfuse.com>
2018-04-15 20:25:27 +0100

    Initial Commit

---
+++ api.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+def callPenton(fieldname, cacheID):
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+    response_data = requests.get('https://vendor2-mock.herokuapp.com/penton/'+fieldname+'/'+cacheID, headers=headers)
+
+    return response_data.json()
+
+def callVerisk(fieldname, cacheID):
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+    response_data = requests.get('https://vendor1-mock.herokuapp.com/verisk/'+fieldname+'/'+cacheID, headers=headers)
+
+    return response_data.json()
+
+def get(event, context):
+    veriskID = event['queryStringParameters']['veriskID']
+    pentonID = event['queryStringParameters']['pentonID']
+    fieldname = 'numEmployees'
+
+    pentonResponse = callPenton(fieldname, pentonID)
+    veriskResponse = callPenton(fieldname, veriskID)
+
+    chosenValue = {};
+    decisionReason = "";
+    chosenVendor = "";
+
+    if(pentonResponse['value'] is not None):
+        chosenValue = pentonResponse
+        decisionReason = 'Penton had a value'
+        chosenVendor = 'Penton'
+    else:
+        chosenValue = veriskResponse
+        decisionReason = 'Penton Didn\'t had a value'
+        chosenVendor = 'Verisk'
+
+    responseDict = {"value": chosenValue['value'],  "chosenVendor": chosenVendor, "decisionReason": decisionReason}
+        
+    return {"body": json.dumps(responseDict, ensure_ascii=False), "statusCode": 200}
